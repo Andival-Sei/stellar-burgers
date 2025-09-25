2e8b834ec508340834a25a818a048fff54f304c2
Kirill Barkov <freedomdragon777@gmail.com>
2025-09-25 17:48:27 +0400

    fix commit

--- src/services/store.test.ts
+++ src/services/store.test.ts
@@ -1,15 +1,12 @@
-import type { AnyAction } from '@reduxjs/toolkit';
 import { rootReducer } from './store';
 
 describe('rootReducer', () => {
   it('должен возвращать корректное начальное состояние при неизвестном экшене', () => {
-    const actualInitialState = rootReducer(undefined, {
-      type: '@@INIT'
-    } as AnyAction);
+    const actualInitialState = rootReducer(undefined, { type: '@@INIT' });
 
     const stateAfterUnknownAction = rootReducer(undefined, {
       type: 'UNKNOWN_ACTION'
-    } as AnyAction);
+    });
 
     expect(stateAfterUnknownAction).toEqual(actualInitialState);
   });
